3d7a0a4384c2e87fe1762b93adabf7b84d4e85dd
Victor Bailiot <victor_bailiot@hotmail.com>
2022-01-04 13:38:09 -0400

    Update landing tabuleiro

--- tabuleiro/js/main.js
+++ tabuleiro/js/main.js
@@ -147,52 +147,6 @@ jQuery(function ($) {
         ]
     });
 
-    $('.case-study-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 200,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [{
-                breakpoint: 1199,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
-
     //Global Form validation
     $('.contact-form').on('submit', function (e) {
         e.preventDefault();
